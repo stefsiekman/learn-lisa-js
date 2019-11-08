e55bd4d03add5d66d8e3beb207dfb4ce6d556b81
Stef Siekman <s.siekman@gmail.com>
2019-11-08 20:58:03 +0100

    Add spinning line

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-const canvas = document.getElementById("canvas")
+function draw() {
+    const ctx = document.getElementById("canvas").getContext("2d")
+
+    ctx.clearRect(0, 0, 300, 400)
+
+    const time = new Date()
+    const seconds = time.getSeconds() + time.getMilliseconds() / 1000
+
+    drawLine(ctx, 200, 150, 150, seconds)
+
+    window.requestAnimationFrame(draw)
+}
+
+draw()
+
+function drawLine(ctx, centerX, centerY, length, rotation) {
+    const xOffset = Math.sin(rotation) * length / 2
+    const yOffset = Math.cos(rotation) * length / 2
+
+    ctx.beginPath()
+    ctx.moveTo(centerX - xOffset, centerY - yOffset)
+    ctx.lineTo(centerX + xOffset, centerY + yOffset)
+    ctx.stroke()
+}
+
 
